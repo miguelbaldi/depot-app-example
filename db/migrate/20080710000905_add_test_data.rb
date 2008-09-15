ab5a6c352df6e4c73997e4bb50f5b5ce0680e05e
miguel aranha baldi horlle <miguelbaldi@miguel-aranha-baldi-horlles-macbook.local>
2008-09-15 03:09:14 -0300

    Test data addition

--- db/migrate/20080710000905_add_test_data.rb
+++ db/migrate/20080710000905_add_test_data.rb
@@ -21,6 +21,23 @@ class AddTestData < ActiveRecord::Migration
 				</p>},
 			:image_url => '/images/ruby.jpg' ,
 			:price => 34.50)
+			
+			Product.create(:title => 'My Job Went to India' ,
+			:description =>
+				%{<p>					
+          This book isn’t about India. It’s about you. You’ve already lost your job. You may still be drawing a paycheck, but the job you were hired to do no longer exists. Your company has changed, the technology has changed, the economy has changed, and the ways you can add value have changed. Have you adapted to these changes? Or are you at risk?
+          You need to architect your career, and this book will show you how.
+				</p>},
+			:image_url => '/images/mjwti.jpg' ,
+			:price => 19.95)
+			
+			Product.create(:title => 'Stripes' ,
+			:description =>
+				%{<p>					
+          Tired of complicated Java web frameworks that just get in your way? Stripes is a lightweight, practical framework that lets you write lean and mean code without a bunch of XML configuration files. Stripes is designed to do a lot of the common work for you, while being flexible enough to adapt to your requirements. This book will show you how to use Stripes to its full potential, so that you can easily develop professional, full-featured web applications. As a bonus, you’ll also get expert advice from the creator of Stripes, Tim Fennell.
+				</p>},
+			:image_url => '/images/fdstr.jpg' ,
+			:price => 19.95)
 			# . . .
 	end
 	def self.down
